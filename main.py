4e7d5dd6871e61b7a4136f05551224ef7f6a2b1f
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-01 20:17:06 -0500

    updated pseudo code

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import data
 
+"""
+run_elo_pipeline pseudo code
+1. get elo seeding dict
+2. iterate through schedule
+    a. reference elo dict for team elos
+    b. compute new elos/win probabilities
+    c. update schedule df
+    d. update elo_dict states
+3. return schedule df
+"""
+
 
 def run_elo_pipeline():
     return
