b4c7ce852c565077695e37f52102f8719fa00f7f
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-01 19:49:29 -0500

    initializing pregame elo to zero

--- data/data.py
+++ data/data.py
@@ -4,7 +4,12 @@ from .nhlApi import get_seasons_games, get_team_names
 
 def generate_games_df() -> pd.DataFrame:
     seasons_games_list = get_seasons_games()
-    return pd.DataFrame(seasons_games_list)
+    df = pd.DataFrame(seasons_games_list)
+
+    df["awayPreGameElo"] = 0
+    df["homePreGameElo"] = 0
+
+    return df
 
 
 def get_teams_list() -> list:
