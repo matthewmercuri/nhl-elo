cde34e0a954f4ec11ddfcb574e32213ef8706ad7
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-03 14:49:33 -0500

    cleanup

--- services.py
+++ services.py
@@ -5,12 +5,14 @@ BASE_ELO = 1200
 
 def get_blank_seeding_dict() -> dict:
     teams_list = data.get_teams_list()
-    seed_dict = dict(zip(teams_list, [BASE_ELO] * len(teams_list)))
+    return dict(zip(teams_list, [BASE_ELO] * len(teams_list)))
 
-    return seed_dict
+
+def get_blank_last_played_dict() -> dict:
+    teams_list = data.get_teams_list()
+    return dict(zip(teams_list, [None] * len(teams_list)))
 
 
 def update_elo_dict(elo_dict: dict, team_name: str, updated_elo: float) -> dict:
     elo_dict.update({team_name: updated_elo})
-
     return elo_dict
