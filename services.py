a41f0cbc832d6021f565a002d1a974a8e34bbb0f
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-03 14:54:55 -0500

    added update function for last game date

--- services.py
+++ services.py
@@ -16,3 +16,10 @@ def get_blank_last_played_dict() -> dict:
 def update_elo_dict(elo_dict: dict, team_name: str, updated_elo: float) -> dict:
     elo_dict.update({team_name: updated_elo})
     return elo_dict
+
+
+def update_last_played_dict(
+    last_played_dict: dict, team_name: str, game_date: str
+) -> dict:
+    last_played_dict.update({team_name: game_date})
+    return last_played_dict
