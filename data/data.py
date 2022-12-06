86cca4780e8e8ab748ea8ca0e826c6157bdb19e1
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-06 17:49:22 -0500

    sorting by date

--- data/data.py
+++ data/data.py
@@ -12,6 +12,8 @@ def generate_games_df(season: str = CURRENT_SEASON_STR) -> pd.DataFrame:
     # INFO: may be able to just reassign to gameDate column
     df["pandasGameDate"] = pd.to_datetime(df["gameDate"], infer_datetime_format=True)
 
+    df.sort_values(by="pandasGameDate", ascending=True, inplace=True)
+
     df["homePreGameElo"] = 0
     df["awayPreGameElo"] = 0
 
