7403f9069e26eb00bbe8fd2d20915d39144e7b4c
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-07 22:13:55 -0500

    added b2b logic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import data
 import elo
 from constants import BASE_ELO, ALLOWED_GAME_TYPES
-from services import get_blank_teams_dict, update_teams_dict
+from services import get_blank_teams_dict, update_teams_dict, has_played_yesterday
 
 """
 TODO:
@@ -9,7 +9,7 @@ TODO:
 """
 
 
-def process_game(game, team_elo_dict: dict):
+def process_game(game, team_elo_dict: dict, last_played_dict: dict):
     if game["gameType"] not in ALLOWED_GAME_TYPES:
         return game
 
@@ -22,8 +22,12 @@ def process_game(game, team_elo_dict: dict):
     game["homePreGameElo"] = prev_home_elo
     game["awayPreGameElo"] = prev_away_elo
 
+    game_date = game["pandasGameDate"]
+    is_home_b2b = has_played_yesterday(last_played_dict, home_name, game_date)
+    is_away_b2b = has_played_yesterday(last_played_dict, away_name, game_date)
+
     home_win_probability, away_win_probability = elo.get_win_probabilities(
-        prev_home_elo, prev_away_elo, False, False
+        prev_home_elo, prev_away_elo, is_home_b2b, is_away_b2b
     )
 
     game["homeWinProbability"] = home_win_probability
@@ -42,14 +46,24 @@ def process_game(game, team_elo_dict: dict):
         update_teams_dict(team_elo_dict, home_name, updated_home_elo)
         update_teams_dict(team_elo_dict, away_name, updated_away_elo)
 
+        update_teams_dict(last_played_dict, home_name, game_date)
+        update_teams_dict(last_played_dict, away_name, game_date)
+
     return game
 
 
 def run_elo_pipeline():
     team_elo_dict = get_blank_teams_dict(BASE_ELO)
+    last_played_dict = get_blank_teams_dict(None)
+
     schedule_df = data.generate_games_df("20212022")
 
-    schedule_df = schedule_df.apply(process_game, axis=1, team_elo_dict=team_elo_dict)
+    schedule_df = schedule_df.apply(
+        process_game,
+        axis=1,
+        team_elo_dict=team_elo_dict,
+        last_played_dict=last_played_dict,
+    )
 
     schedule_df.to_csv("test.csv")
 
--- services.py
+++ services.py
@@ -1,3 +1,4 @@
+import datetime
 import data
 from typing import Any
 
@@ -13,5 +14,17 @@ def update_teams_dict(elo_dict: dict, team_name: str, updated_value: Any) -> dic
 
 
 # TODO: adjust type of current_game_date
-def has_played_yesterday(last_played_dict: dict, current_game_date: Any) -> bool:
+def has_played_yesterday(
+    last_played_dict: dict, team_name: str, current_game_date: Any
+) -> bool:
+    if last_played_dict[team_name] is None:
+        return False
+
+    delta = current_game_date - last_played_dict[team_name]
+    days = delta.days
+    hours = delta.seconds / (60 * 60)
+
+    if days < 1 or (days == 1 and hours == 0):
+        return True
+
     return False
